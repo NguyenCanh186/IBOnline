76289feb12d545773d8ef2cb094fe15e41125fd2
Hồ Ngọc Duẩn <ngocduan.ho@vmgmedia.vn>
2024-01-15 03:30:05 +0000

    Fix/create user

--- src/main/java/com/vmg/ibo/customer/model/customer/RegisterModel.java
+++ src/main/java/com/vmg/ibo/customer/model/customer/RegisterModel.java
@@ -6,18 +6,22 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 public class RegisterModel {
     @NotNull(message = "Email bắt buộc nhập!", groups = {Insert.class, Update.class})
+    @Size(max = 64, message = "Tên email không dài quá 64 ký tự.", groups = {Insert.class, Update.class})
     private String email;
 
     @NotNull(message = "Mật khẩu bắt buộc nhập!", groups = {Insert.class, Update.class})
+    @Size(max = 128, message = "Bạn đã nhập quá 128 ký tự cho phép. Vui lòng nhập lại.", groups = {Insert.class, Update.class})
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=])[A-Za-z\\d@#$%^&+=]{8,}$",
             message = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm ít nhất một chữ thường, một chữ hoa, một số, và một ký tự đặc biệt.", groups = {Insert.class, Update.class})
     private String password;
 
     @NotNull(message = "Xác nhận mật khẩu bắt buộc nhập!", groups = {Insert.class, Update.class})
+    @Size(max = 128, message = "Bạn đã nhập quá 128 ký tự cho phép. Vui lòng nhập lại.", groups = {Insert.class, Update.class})
     private String confirmPassword;
 
     public RegisterModel(String email, String password, String confirmPassword) {
