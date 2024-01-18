20375f8cb015d78e376e4020f41b2f54c8a4bac7
ngocduan1803 <ngocduan.ho@vmgmedia.vn>
2024-01-18 11:56:48 +0700

    fix view data detail demand

---
+++ src/main/java/com/vmg/ibo/form/dto/TemplateDTO.java
@@ -0,0 +1,12 @@
+package com.vmg.ibo.form.dto;
+
+import lombok.Data;
+
+@Data
+public class TemplateDTO {
+    private String name;
+    private String slug;
+    private String tag;
+    private Integer type;
+    private String image;
+}
