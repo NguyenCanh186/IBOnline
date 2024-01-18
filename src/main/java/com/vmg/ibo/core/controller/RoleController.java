de46fbc742c3e4e95e96809f981fbfc8be0e698a
Nguyễn Thế Cảnh <thecanh.nguyen@vmgmedia.vn>
2024-01-18 09:19:47 +0000

    Feat/add form demand

--- src/main/java/com/vmg/ibo/core/controller/RoleController.java
+++ src/main/java/com/vmg/ibo/core/controller/RoleController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/roles")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class RoleController {
     @Autowired
     private IRoleService roleService;
