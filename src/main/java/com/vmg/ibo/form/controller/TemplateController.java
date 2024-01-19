968b21892692f18e90fe8c3819c493257aacff8c
Nguyễn Thế Cảnh <thecanh.nguyen@vmgmedia.vn>
2024-01-19 11:36:51 +0700

    Cấu trúc lại cơ sở dữ liệu

--- src/main/java/com/vmg/ibo/form/controller/TemplateController.java
+++ src/main/java/com/vmg/ibo/form/controller/TemplateController.java
@@ -1,10 +1,8 @@
 package com.vmg.ibo.form.controller;
 
 import com.vmg.ibo.core.base.Result;
-import com.vmg.ibo.form.dto.FormDTO;
 import com.vmg.ibo.form.entity.Template;
 import com.vmg.ibo.form.model.FormDataReq;
-import com.vmg.ibo.form.service.form.IFormService;
 import com.vmg.ibo.form.service.template.ITemplateService;
 import com.vmg.ibo.form.service.form_field.IFormFieldService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,15 +18,16 @@ public class TemplateController {
     private IFormFieldService formFieldService;
 
     @Autowired
-    private IFormService iFormService;
-
-    @Autowired
     private ITemplateService templateService;
 
     @GetMapping("/get-template/{id}")
     @PreAuthorize("hasAuthority('get-form')")
     public Result<?> getFormsFieldById(@PathVariable Long id) {
-        return Result.success("Lấy dữ liệu thành công", templateService.getTemplateById(id));
+        Optional<Template> template = templateService.getTemplateById(id);
+        if (!template.isPresent()) {
+            return Result.error(404, "Không tìm thấy template");
+        }
+        return Result.success("Lấy dữ liệu thành công", template.get()) ;
     }
 
     @GetMapping("/get-template")
