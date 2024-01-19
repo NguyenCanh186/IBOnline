2797794012f0d0624cdd8e77a0d9a6f3a18da79c
Nguyễn Thế Cảnh <thecanh.nguyen@vmgmedia.vn>
2024-01-19 14:01:59 +0700

    Cấu trúc lại cơ sở dữ liệu

--- src/main/java/com/vmg/ibo/core/repository/IRoleRepository.java
+++ src/main/java/com/vmg/ibo/core/repository/IRoleRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 public interface IRoleRepository extends JpaRepository<Role, Long> {
 
-    @Query(value = "SELECT  r FROM Role r where (:name is null OR lower(r.name) LIKE  %:name% or lower(r.code) LIKE %:name%) ")
+    @Query(value = "SELECT  r FROM Role r where (:name is null OR lower(r.name) LIKE  %:name% or lower(r.code) LIKE %:name%) AND r.code != 'KH'")
     Page<Role> findAllByNameContaining(String name, Pageable pageable);
 
-    @Query("SELECT r FROM Role r WHERE r.status = 1")
+    @Query("SELECT r FROM Role r WHERE r.status = 1 AND r.code != 'KH'")
     List<Role> findAllRoleActive();
     Role findByMerchantDefault(Integer merchantDefault);
 }
