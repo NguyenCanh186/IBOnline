4ff6e975433049f0b7188fbcaaf4b11182d2fb99
Nguyễn Thế Cảnh <thecanh.nguyen@vmgmedia.vn>
2024-01-24 11:10:44 +0700

    Cấu trúc lại cơ sở dữ liệu

--- src/main/java/com/vmg/ibo/form/repository/ITemplateFieldRepository.java
+++ src/main/java/com/vmg/ibo/form/repository/ITemplateFieldRepository.java
@@ -4,6 +4,9 @@ import com.vmg.ibo.form.entity.TemplateField;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ITemplateFieldRepository extends JpaRepository<TemplateField, Long> {
+    List<TemplateField> getTemplateFieldsByTemplateId(Long templateId);
 }
